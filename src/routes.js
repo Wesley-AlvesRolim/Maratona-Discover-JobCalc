5eee860b7b86399cb1ab67bc71b7896b02ef9007
Wesley-AlvesRolim <walvesrolim@gmail.com>
2021-03-30 15:35:43 -0300

    First class done

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const routes = express.Router();
+const views = `${__dirname}/views/`
+
+const profile = {
+    name: 'Wesley',
+    avatar: 'https://avatars.githubusercontent.com/u/78855198?v=4',
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 6,
+    "vacation-per-year": 4,
+}
 
 routes.get('/', (request, response) => {
     // __dirname é uma variavel de caminho absoluto.
-    return response.sendFile(`${__dirname}/views/index.html`)
+    return response.render(`${views}index`)
 });
-routes.get('/', (request, response) => {
-    // __dirname é uma variavel de caminho absoluto.
-    return response.sendFile(`${__dirname}/views/job.html`)
+routes.get('/job/edit', (request, response) => {
+    return response.render(`${views}job-edit`)
+});
+routes.get('/job', (request, response) => {
+    return response.render(`${views}job`)
+});
+routes.get('/profile', (request, response) => {
+    return response.render(`${views}profile`, { profile })
 });
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const server = express();
-const routes = require('./routes')
+const routes = require('./routes');
+server.set('view engine', 'ejs')
     //definindo uma rota 'padrao' a partir dai vai ser criado todas as rotas
 server.use(express.static('public'))
 server.use(routes)
-server.listen(5500, () => {})
+server.listen(5500)
