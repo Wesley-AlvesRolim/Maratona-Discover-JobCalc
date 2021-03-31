3dd08a031431c2a5421f595f9688304db1e18cfc
Wesley-AlvesRolim <walvesrolim@gmail.com>
2021-03-31 18:01:32 -0300

    Class at 31/03/2021

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const routes = express.Router();
 const views = `${__dirname}/views/`
-
+const jobs = [{
+        id: 1,
+        name: 'Pizzaria Guloso',
+        "daily-hours": 6,
+        "total-hours": 80,
+        createdAt: Date.now()
+    },
+    {
+        id: 2,
+        name: 'OneTwo Project',
+        "daily-hours": 3,
+        "total-hours": 40,
+        createdAt: Date.now()
+    }
+];
 const profile = {
     name: 'Wesley',
     avatar: 'https://avatars.githubusercontent.com/u/78855198?v=4',
@@ -13,15 +27,28 @@ const profile = {
 
 routes.get('/', (request, response) => {
     // __dirname é uma variavel de caminho absoluto.
-    return response.render(`${views}index`)
-});
-routes.get('/job/edit', (request, response) => {
-    return response.render(`${views}job-edit`)
+    return response.render(`${views}index`, { jobs })
 });
 routes.get('/job', (request, response) => {
     return response.render(`${views}job`)
 });
+routes.post('/job', (request, response) => {
+    const id = jobs[jobs.length - 1].id + 1 || 1
+    const valuesInInputs = {
+        id: id,
+        name: request.body.name,
+        "daily-hours": request.body["daily-hours"],
+        "total-hours": request.body["total-hours"],
+        createdAt: Date.now()
+    };
+    jobs.push(valuesInInputs);
+    return response.redirect('/')
+});
+routes.get('/job/edit', (request, response) => {
+    return response.render(`${views}job-edit`)
+});
 routes.get('/profile', (request, response) => {
     return response.render(`${views}profile`, { profile })
 });
+
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const server = express();
 const routes = require('./routes');
-server.set('view engine', 'ejs')
-    //definindo uma rota 'padrao' a partir dai vai ser criado todas as rotas
-server.use(express.static('public'))
-server.use(routes)
-server.listen(5500)
+const urlencoded = express.urlencoded({ extended: true });
+
+server.set('view engine', 'ejs');
+//definindo uma rota 'padrao' a partir dai vai ser criado todas as rotas
+server.use(express.static('public'));
+server.use(urlencoded);
+server.use(routes);
+server.listen(5500);
